c4e31c6c380284e1391e0bd40abe7c41260e2a47
Filip <filip.trifunovic23@gmail.com>
2022-10-20 13:52:18 +0200

    adding components

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import SearchForm from "./SearchForm.js";
+import Stories from "./Stories.js";
+import Buttons from "./Buttons.js";
+
+function App() {
+  return <h2>News</h2>;
+}
+
+export default App;
---
+++ src/Buttons.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+
+function Buttons() {
+  return <div>Buttons</div>;
+}
+
+export default Buttons;
